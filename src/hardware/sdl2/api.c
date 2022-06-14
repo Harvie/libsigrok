7898158b654a45c1d3c5b24888de5ae07c4e26a3
Tomas Mudrunka <tomas@mudrunka.cz>
2022-06-14 15:27:50 +0200

    Major SDL2 cleanup

--- src/hardware/sdl2/api.c
+++ src/hardware/sdl2/api.c
@@ -1,9 +1,7 @@
 /*
  * This file is part of the libsigrok project.
  *
- * Copyright (C) 2011 Daniel Ribeiro <drwyrm@gmail.com>
- * Copyright (C) 2012 Renato Caldas <rmsc@fe.up.pt>
- * Copyright (C) 2013 Lior Elazary <lelazary@yahoo.com>
+ * Copyright (C) 2022 Tomas Mudrunka <harviecz@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,29 +24,27 @@
 static const uint32_t drvopts[] = {
 	//SR_CONF_DEMO_DEV,
 	SR_CONF_OSCILLOSCOPE,
-	SR_CONF_LOGIC_ANALYZER,
+	//SR_CONF_LOGIC_ANALYZER,
 };
 
 static const uint32_t devopts[] = {
 	SR_CONF_LIMIT_SAMPLES | SR_CONF_SET,
-	SR_CONF_SAMPLERATE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+	SR_CONF_SAMPLERATE | SR_CONF_GET, // | SR_CONF_SET | SR_CONF_LIST,
+/*
 	SR_CONF_TRIGGER_MATCH | SR_CONF_LIST,
 	SR_CONF_TRIGGER_SLOPE | SR_CONF_SET,
 	SR_CONF_HORIZ_TRIGGERPOS | SR_CONF_SET,
 	SR_CONF_CAPTURE_RATIO | SR_CONF_SET,
 	SR_CONF_RLE | SR_CONF_SET,
+*/
+};
+
+static const char *channel_names[] = {
+	//5.1 Audio: Front-Left, Front-Right, Center, LowFreq, Surround-Left, Surround-Right
+	"FL", "FR", "CE", "LF", "SL", "SR"
 };
 
 /*
- * Channels are numbered 0 to 7.
- *
- * See also: http://www.linkinstruments.com/images/mso19_1113.gif
- */
-static const char *channel_names[] = {
-	/* Note: DSO needs to be first. */
-	"DSO", "0", "1", "2", "3", "4", "5", "6", "7",
-};
-
 static const uint64_t samplerates[] = {
 	SR_HZ(100),
 	SR_MHZ(200),
@@ -58,6 +54,7 @@ static const uint64_t samplerates[] = {
 static const char *trigger_slopes[2] = {
 	"r", "f",
 };
+*/
 
 static int init(struct sr_dev_driver *di, struct sr_context *sr_ctx) {
 	SDL_Init(SDL_INIT_AUDIO);
@@ -78,37 +75,28 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
         struct sr_dev_inst *sdi;
         struct sr_channel *ch;
         struct sr_channel_group *cg, *acg;
-/*
-        struct sr_config *src;
-        struct analog_gen *ag;
-        GSList *l;
-        int num_logic_channels, num_analog_channels, pattern, i;
-        uint64_t limit_frames;
-        char channel_name[16];
 
-        num_logic_channels = DEFAULT_NUM_LOGIC_CHANNELS;
-        num_analog_channels = DEFAULT_NUM_ANALOG_CHANNELS;
-        limit_frames = DEFAULT_LIMIT_FRAMES;
-*/
+	int dev_count = SDL_GetNumAudioDevices(0);
+        SDL_AudioDeviceID	dev_i;
+	SDL_AudioSpec		dev_spec;
 
-        int dev_i, dev_count = SDL_GetNumAudioDevices(0);
-	SDL_AudioSpec dev_spec;
       for (dev_i = 0; dev_i < dev_count; ++dev_i) {
                 //printf("Audio device %d: %s\n", i, SDL_GetAudioDeviceName(i, 0));
 
 		if(SDL_GetAudioDeviceSpec(dev_i, 1, &dev_spec)) continue;
 
+	//Create driver specific data (priv) structure for driver instance
+        devc = g_malloc0(sizeof(struct dev_context));
+ 	memcpy(&devc->sdl_device_spec, &dev_spec, sizeof(SDL_AudioSpec));
+	devc->sdl_device_index = dev_i;
+        devc->cur_samplerate = SR_HZ(devc->sdl_device_spec.freq);
+
 	//Create device instance
         sdi = g_malloc0(sizeof(struct sr_dev_inst));
         sdi->status = SR_ST_INACTIVE;
         sdi->model = g_strdup_printf("SDL2 Soundcard %d (%d x %d Hz): %s", dev_i, dev_spec.channels, dev_spec.freq, SDL_GetAudioDeviceName(dev_i, 0));
-	devices = g_slist_append(devices, sdi);
-
-	//Put driver specific data to driver instance
-        devc = g_malloc0(sizeof(struct dev_context));
-	devc->sdl_device_index = dev_i;
-        devc->cur_samplerate = SR_HZ(dev_spec.freq);
-        sdi->priv = devc;
+        sdi->priv = devc; //Reference to driver specific data
+	devices = g_slist_append(devices, sdi); //Add device to list
 
         //Create analog channel group
         acg = g_malloc0(sizeof(struct sr_channel_group));
@@ -118,7 +106,7 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 	int ch_i;
 	for(ch_i=0;ch_i < dev_spec.channels;ch_i++) {
 		//Put new channel to group
-        	ch = sr_channel_new(sdi, ch_i, SR_CHANNEL_ANALOG, TRUE, "A");
+        	ch = sr_channel_new(sdi, ch_i, SR_CHANNEL_ANALOG, TRUE, channel_names[ch_i]);
         	acg->channels = g_slist_append(acg->channels, ch);
 	}
 
@@ -126,12 +114,6 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 
 
         return std_scan_complete(di, devices);
-
-
-
-
-
-
 
 }
 
@@ -146,25 +128,7 @@ static int dev_open(struct sr_dev_inst *sdi)
 	SDL_AudioSpec dev_spec;
 	if(SDL_GetAudioDeviceSpec(devc->sdl_device_index, 1, &dev_spec)) return SR_ERR;
 
-
-	//if (serial_open(devc->serial, SERIAL_RDWR) != SR_OK)
-	//	return SR_ERR;
-
-	/* FIXME: discard serial buffer */
-	//mso_check_trigger(devc->serial, &devc->trigger_state);
-	//sr_dbg("Trigger state: 0x%x.", devc->trigger_state);
-
-	//ret = mso_reset_adc(sdi);
-	//if (ret != SR_OK)
-	//	return ret;
-
-	//mso_check_trigger(devc->serial, &devc->trigger_state);
-	//sr_dbg("Trigger state: 0x%x.", devc->trigger_state);
-
-	//    ret = mso_reset_fsm(sdi);
-	//    if (ret != SR_OK)
-	//            return ret;
-	//    return SR_ERR;
+	//TODO: flush buffer?
 
 	return SR_OK;
 }
@@ -183,7 +147,7 @@ static int config_get(int key, GVariant **data,
 
 	switch (key) {
 	case SR_CONF_SAMPLERATE:
-		*data = g_variant_new_uint64(devc->cur_rate);
+		*data = g_variant_new_uint64(devc->cur_samplerate);
 		break;
 	default:
 		return SR_ERR_NA;
@@ -213,11 +177,8 @@ static int config_set(int key, GVariant *data,
 		return mso_configure_rate(sdi, g_variant_get_uint64(data));
 	case SR_CONF_LIMIT_SAMPLES:
 		num_samples = g_variant_get_uint64(data);
-		if (num_samples != 1024) {
-			sr_err("Only 1024 samples are supported. (%d given)", num_samples);
-			//return SR_ERR_ARG;
-		}
 		devc->limit_samples = num_samples;
+		sr_err("Received config samples: %lu", num_samples);
 		break;
 	case SR_CONF_CAPTURE_RATIO:
 		break;
@@ -233,7 +194,7 @@ static int config_set(int key, GVariant *data,
 			return SR_ERR_ARG;
 		}
 		trigger_pos = (int)pos;
-		devc->trigger_holdoff[0] = trigger_pos & 0xff;
+		//devc->trigger_holdoff[0] = trigger_pos & 0xff;
 		break;
 	case SR_CONF_RLE:
 		break;
@@ -252,10 +213,10 @@ static int config_list(int key, GVariant **data,
 	case SR_CONF_DEVICE_OPTIONS:
 		return STD_CONFIG_LIST(key, data, sdi, cg, NO_OPTS, drvopts, devopts);
 	case SR_CONF_SAMPLERATE:
-		*data = std_gvar_samplerates_steps(ARRAY_AND_SIZE(samplerates));
+		// *data = std_gvar_samplerates_steps(ARRAY_AND_SIZE(samplerates));
 		break;
 	case SR_CONF_TRIGGER_MATCH:
-		*data = g_variant_new_string(TRIGGER_TYPE);
+		// *data = g_variant_new_string(TRIGGER_TYPE);
 		break;
 	default:
 		return SR_ERR_NA;
@@ -264,10 +225,7 @@ static int config_list(int key, GVariant **data,
 	return SR_OK;
 }
 
-SR_PRIV int sdl_prepare_data(int fd, int revents, void *cb_data) {
-	static long int counter=0;
-	counter++;
-	if(counter > 50) return SR_OK;
+SR_PRIV int sdl_data_callback(int fd, int revents, void *cb_data) {
 
 	struct sr_dev_inst *sdi;
         struct dev_context *devc;
@@ -280,13 +238,16 @@ SR_PRIV int sdl_prepare_data(int fd, int revents, void *cb_data) {
 
 	struct sr_rational r_scale, r_offset;
 	r_scale.p = 1;
-	r_scale.q = 1;
+	r_scale.q = 64;
 	r_offset.p = 0;
 	r_offset.q = 1;
 
-
 	sdi = cb_data;
 	devc = sdi->priv;
+
+	sr_err("Samples: %lu", devc->limit_samples_remaining);
+	if(devc->limit_samples_remaining <= 0) return SR_OK; //Already sent everything
+
 
 	sr_analog_init(&packet_analog, &encoding, &meaning, &spec, 0);
 
@@ -315,19 +276,19 @@ SR_PRIV int sdl_prepare_data(int fd, int revents, void *cb_data) {
 	meaning.channels = lastcg->channels;
 
 	//data
-	char data[1024]={60,60,0,0,-60,-60,0,0};
+	char data[1024]={60,127,0,127,-60,-60,0,0};
 	packet_analog.data = data;
 	packet_analog.num_samples=4;
 	packet_analog.encoding = &encoding;
 	packet_analog.meaning = &meaning;
 	packet_analog.spec = &spec;
 
-
 	//packet
         packet.type = SR_DF_ANALOG;
         packet.payload = &packet_analog;
 
         sr_session_send(sdi, &packet);
+	devc->limit_samples_remaining -= packet_analog.num_samples;
 
 	return G_SOURCE_CONTINUE;
 	//return SR_OK;
@@ -340,76 +301,25 @@ static int dev_acquisition_start(const struct sr_dev_inst *sdi)
 
 	devc = sdi->priv;
 
-/*
+	sr_err("Limiting samples to %lu", devc->limit_samples);
+	devc->limit_samples_remaining = devc->limit_samples;
 
-	if (mso_configure_channels(sdi) != SR_OK) {
-		sr_err("Failed to configure channels.");
-		return SR_ERR;
-	}
-
-	// FIXME: No need to do full reconfigure every time
-//      ret = mso_reset_fsm(sdi);
-//      if (ret != SR_OK)
-//              return ret;
-
-	// FIXME: ACDC Mode
-	devc->ctlbase1 &= 0x7f;
-//      devc->ctlbase1 |= devc->acdcmode;
-
-	ret = mso_configure_rate(sdi, devc->cur_rate);
-	if (ret != SR_OK)
-		return ret;
-
-	// set dac offset
-	ret = mso_dac_out(sdi, devc->dac_offset);
-	if (ret != SR_OK)
-		return ret;
-
-	ret = mso_configure_threshold_level(sdi);
-	if (ret != SR_OK)
-		return ret;
-
-	//ret = mso_configure_trigger(sdi);
-	//if (ret != SR_OK)
-	//	return ret;
-
-	// END of config hardware part
-	ret = mso_arm(sdi);
-	if (ret != SR_OK)
-		return ret;
-
-	// Start acquisition on the device
-	//mso_check_trigger(devc->serial, &devc->trigger_state);
-	//ret = mso_check_trigger(devc->serial, NULL);
-	//if (ret != SR_OK)
-	//	return ret;
-
-	// Reset trigger state.
-	//devc->trigger_state = 0x00;
+	sr_session_source_add(sdi->session, -1, 0, 100, sdl_data_callback, (struct sr_dev_inst *)sdi);
 
 	std_session_send_df_header(sdi);
-
-	// Our first channel is analog, the other 8 are of type 'logic'.
-
-	serial_source_add(sdi->session, devc->serial, G_IO_IN, -1,
-			mso_receive_data, sdi);
-*/
-
-	sr_session_source_add(sdi->session, -1, 0, 100, sdl_prepare_data, (struct sr_dev_inst *)sdi);
-
-	std_session_send_df_header(sdi);
-
 
 
 	return SR_OK;
 }
 
+/*
 static int dev_acquisition_stop(struct sr_dev_inst *sdi)
 {
 	stop_acquisition(sdi);
 
 	return SR_OK;
 }
+*/
 
 static struct sr_dev_driver sdl2_driver_info = {
 	.name = "sdl2",
@@ -434,7 +344,7 @@ static struct sr_dev_driver sdl2_driver_info = {
 
 	//acq
 	.dev_acquisition_start = dev_acquisition_start,
-	.dev_acquisition_stop = dev_acquisition_stop,
+	.dev_acquisition_stop = std_session_send_df_end, //dev_acquisition_stop,
 
 	//inst
 	.context = NULL,
--- src/hardware/sdl2/protocol.h
+++ src/hardware/sdl2/protocol.h
@@ -19,230 +19,30 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef LIBSIGROK_HARDWARE_LINK_MSO19_PROTOCOL_H
-#define LIBSIGROK_HARDWARE_LINK_MSO19_PROTOCOL_H
+#ifndef LIBSIGROK_HARDWARE_SDL2_PROTOCOL_H
+#define LIBSIGROK_HARDWARE_SDL2_PROTOCOL_H
 
 #include <stdint.h>
 #include <string.h>
 #include <glib.h>
-#include <libudev.h>
+//#include <libudev.h>
 #include <libsigrok/libsigrok.h>
 #include "libsigrok-internal.h"
+#include <SDL2/SDL.h>
 
-#define LOG_PREFIX "sdl2logprefix"
+#define LOG_PREFIX "sdl2-audio-interface"
 
-#define USB_VENDOR		"3195"
-#define USB_PRODUCT		"f190"
-
-#define NUM_TRIGGER_STAGES	4
-#define TRIGGER_TYPE 		"01"	//the first r/f is used for the whole group
-#define SERIALCOMM		"460800/8n1/flow=2"
-#define SERIALCONN		"/dev/ttyUSB0"
-#define CLOCK_RATE		SR_MHZ(100)
-#define MIN_NUM_SAMPLES		4
-
-#define MSO_TRIGGER_UNKNOWN	'!'
-#define MSO_TRIGGER_UNKNOWN1	'1'
-#define MSO_TRIGGER_UNKNOWN2	'2'
-#define MSO_TRIGGER_UNKNOWN3	'3'
-#define MSO_TRIGGER_WAIT	'4'
-#define MSO_TRIGGER_FIRED	'5'
-#define MSO_TRIGGER_DATAREADY	'6'
-
-enum trigger_slopes {
-	SLOPE_POSITIVE = 0,
-	SLOPE_NEGATIVE,
-};
-
-/* Structure for the pattern generator state */
-struct mso_patgen {
-	/* Pattern generator clock config */
-	uint16_t clock;
-	/* Buffer start address */
-	uint16_t start;
-	/* Buffer end address */
-	uint16_t end;
-	/* Pattern generator config */
-	uint8_t config;
-	/* Samples buffer */
-	uint8_t buffer[1024];
-	/* Input/output configuration for the samples buffer (?) */
-	uint8_t io[1024];
-	/* Number of loops for the pattern generator */
-	uint8_t loops;
-	/* Bit enable mask for the I/O lines */
-	uint8_t mask;
-};
-
-/* Data structure for the protocol trigger state */
-struct mso_prototrig {
-	/* Word match buffer */
-	uint8_t word[4];
-	/* Masks for the wordmatch buffer */
-	uint8_t mask[4];
-	/* SPI mode 0, 1, 2, 3. Set to 0 for I2C */
-	uint8_t spimode;
-};
 
 struct dev_context {
-	int sdl_device_index;
-
-	const struct fx2lafw_profile *profile;
-	GSList *enabled_analog_channels;
-	/*
-	 * Since we can't keep track of an fx2lafw device after upgrading
-	 * the firmware (it renumerates into a different device address
-	 * after the upgrade) this is like a global lock. No device will open
-	 * until a proper delay after the last device was upgraded.
-	 */
-	int64_t fw_updated;
-
-	const uint64_t *samplerates;
-	int num_samplerates;
+	SDL_AudioDeviceID	sdl_device_index;
+	SDL_AudioSpec		sdl_device_spec;
 
 	uint64_t cur_samplerate;
 	uint64_t limit_samples;
+	uint64_t limit_samples_remaining;
 	uint64_t capture_ratio;
-
-	gboolean trigger_fired;
-	gboolean acq_aborted;
-	gboolean sample_wide;
-	struct soft_trigger_logic *stl;
-
-	unsigned int sent_samples;
-	int submitted_transfers;
-	int empty_transfer_count;
-
-	unsigned int num_transfers;
-	struct libusb_transfer **transfers;
-	struct sr_context *ctx;
-	void (*send_data_proc)(struct sr_dev_inst *sdi,
-		uint8_t *data, size_t length, size_t sample_width);
-	uint8_t *logic_buffer;
-	float *analog_buffer;
-
-
-
-
-
-// stary mso19 ---------------------------
-
-       /* info */
-       uint8_t hwmodel;
-       uint8_t hwrev;
-       struct sr_serial_dev_inst *serial;
-//      uint8_t num_sample_rates;
-       /* calibration */
-       double vbit;
-       uint16_t dac_offset;
-       uint16_t offset_range;
-
-       uint64_t num_samples;
-
-       /* register cache */
-       uint8_t ctlbase1;
-       uint8_t ctlbase2;
-
-       uint8_t la_threshold;
-       uint64_t cur_rate;
-       uint8_t dso_probe_attn;
-       int8_t use_trigger;
-       uint8_t trigger_chan;
-       uint8_t trigger_slope;
-       uint8_t trigger_outsrc;
-       uint8_t trigger_state;
-       uint8_t trigger_holdoff[2];
-       uint8_t la_trigger;
-       uint8_t la_trigger_mask;
-       double dso_trigger_voltage;
-       uint16_t dso_trigger_width;
-       struct mso_prototrig protocol_trigger;
-       uint16_t buffer_n;
-       char buffer[4096];
-
 };
 
-SR_PRIV int mso_parse_serial(const char *iSerial, const char *iProduct,
-			     struct dev_context *ctx);
-//SR_PRIV int mso_check_trigger(struct sr_serial_dev_inst *serial, uint8_t * info);
-SR_PRIV int mso_reset_adc(struct sr_dev_inst *sdi);
-SR_PRIV int mso_clkrate_out(struct sr_serial_dev_inst *serial, uint16_t val);
-SR_PRIV int mso_configure_rate(const struct sr_dev_inst *sdi, uint32_t rate);
-SR_PRIV int mso_receive_data(int fd, int revents, void *cb_data);
-//SR_PRIV int mso_configure_trigger(const struct sr_dev_inst *sdi);
-SR_PRIV int mso_configure_threshold_level(const struct sr_dev_inst *sdi);
-SR_PRIV int mso_read_buffer(struct sr_dev_inst *sdi);
-SR_PRIV int mso_arm(const struct sr_dev_inst *sdi);
-SR_PRIV int mso_force_capture(struct sr_dev_inst *sdi);
-SR_PRIV int mso_dac_out(const struct sr_dev_inst *sdi, uint16_t val);
-SR_PRIV uint16_t mso_calc_raw_from_mv(struct dev_context *devc);
-SR_PRIV int mso_reset_fsm(struct sr_dev_inst *sdi);
-SR_PRIV int mso_toggle_led(struct sr_dev_inst *sdi, int state);
-
-SR_PRIV int mso_configure_channels(const struct sr_dev_inst *sdi);
-SR_PRIV void stop_acquisition(const struct sr_dev_inst *sdi);
-
-/* bank agnostic registers */
-#define REG_CTL2		15
-
-/* bank 0 registers */
-#define REG_BUFFER		1
-#define REG_TRIGGER		2
-#define REG_CLKRATE1		9
-#define REG_CLKRATE2		10
-#define REG_DAC1		12
-#define REG_DAC2		13
-/* possibly bank agnostic: */
-#define REG_CTL1		14
-
-/* bank 2 registers (SPI/I2C protocol trigger) */
-#define REG_PT_WORD(x)		(x)
-#define REG_PT_MASK(x)		(x + 4)
-#define REG_PT_SPIMODE		8
-
-/* bits - REG_CTL1 */
-#define BIT_CTL1_RESETFSM	(1 << 0)
-#define BIT_CTL1_ARM		(1 << 1)
-#define BIT_CTL1_ADC_UNKNOWN4	(1 << 4)	/* adc enable? */
-#define BIT_CTL1_RESETADC	(1 << 6)
-#define BIT_CTL1_LED		(1 << 7)
-
-/* bits - REG_CTL2 */
-#define BITS_CTL2_BANK(x)	(x & 0x3)
-#define BIT_CTL2_SLOWMODE	(1 << 5)
-
-struct rate_map {
-	uint32_t rate;
-	uint16_t val;
-	uint8_t slowmode;
-};
-
-static const struct rate_map rate_map[] = {
-	{ SR_MHZ(200), 0x0205, 0 },
-	{ SR_MHZ(100), 0x0105, 0 },
-	{ SR_MHZ(50),  0x0005, 0 },
-	{ SR_MHZ(20),  0x0303, 0 },
-	{ SR_MHZ(10),  0x0308, 0 },
-	{ SR_MHZ(5),   0x030c, 0 },
-	{ SR_MHZ(2),   0x0330, 0 },
-	{ SR_MHZ(1),   0x0362, 0 },
-	{ SR_KHZ(500), 0x03c6, 0 },
-	{ SR_KHZ(200), 0x07f2, 0 },
-	{ SR_KHZ(100), 0x0fe6, 0 },
-	{ SR_KHZ(50),  0x1fce, 0 },
-	{ SR_KHZ(20),  0x4f86, 0 },
-	{ SR_KHZ(10),  0x9f0e, 0 },
-	{ SR_KHZ(5),   0x03c7, 0x20 },
-	{ SR_KHZ(2),   0x07f3, 0x20 },
-	{ SR_KHZ(1),   0x0fe7, 0x20 },
-	{ SR_HZ(500),  0x1fcf, 0x20 },
-	{ SR_HZ(200),  0x4f87, 0x20 },
-	{ SR_HZ(100),  0x9f0f, 0x20 },
-};
-
-/* FIXME: Determine corresponding voltages */
-static const uint16_t la_threshold_map[] = {
-	0x8600, 0x8770, 0x88ff, 0x8c70, 0x8eff, 0x8fff,
-};
+//SR_PRIV void stop_acquisition(const struct sr_dev_inst *sdi);
 
 #endif
